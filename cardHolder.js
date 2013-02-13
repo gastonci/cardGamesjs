c2f46bdf5b7402ce54a8cfce943539fc3e2f5cf7
gaston galante <aphardsoft@yahoo.com.ar>
2013-02-13 19:57:54 -0300

    re-submit work
    bug con la app de github

--- cardHolder.js
+++ cardHolder.js
@@ -35,7 +35,7 @@
 	}
 	this.create = function(top,left,width,height,from,to,expand,arrange)
 	{
-		var res = {},addFn = 'var card = arguments[0];card.top = this.top;card.left = this.left;this.child.push(card);',
+		var res = {},addFn = 'var card = arguments[0];this.child.push(card);',
 		subtractFn = '';
 		res.size = 0;
 		res.baseIndex = 0;
@@ -45,20 +45,37 @@
 		res.right = width + left;
 		res.child = [];
 		res.childXY = [];
-		switch(from){
+		switch(from.vertical){
 			case 'left':
-				addFn += 'card.style.left = this.left;this.childXY.push({left:this.left,top:this.top});card.style.top = this.top;'
+				res.from = res.left;
+				addFn += 'card.style.left = this.fromX;';
+				break;
+			case 'right':
+				res.from = null;
+				addFn += 'this.fromX = this.fromX?this.fromX:(this.right - parseInt(card.style.width));card.style.left = this.fromX;';
 				break;
 		}
+		switch(from.horizontal){
+			case 'top':
+				res.fromY = res.top;
+				addFn += 'card.style.top = this.fromY;';
+				break;
+			case 'bottom':
+				addFn += 'this.fromY = this.down - parseInt(card.style.height);card.style.top = this.fromY;';
+				break;
+		}
+		addFn += 'this.childXY.push({left:this.fromX,top:this.fromY,index:this.baseIndex});'
 		switch(to){
 			case 'left':
 				res.baseIndex = 100;
-				res.toLimit = res.left;
-				addFn += 'card.style.zIndex = this.baseIndex;this.left -= '+expand+';this.baseIndex--;';
+				addFn += 'card.style.zIndex = this.baseIndex;this.fromX -= '+(from.vertical == to?expand+';this.left -= '+expand:expand)+';this.baseIndex--;';
+				if(from == 'right'){addFn += 'if(this.fromX <= this.left)this.left = this.fromX;';}
 				break;
 			case 'right':
-				res.toLimit = res.right;
-				addFn += 'card.style.zIndex = this.baseIndex;this.left += '+expand+';this.baseIndex++;';
+				addFn += 'card.style.zIndex = this.baseIndex;this.fromX += '+(from.vertical == to?expand+';this.right += '+expand:expand)+';this.baseIndex++;';
+				break;
+		}
+		switch(to){
 		}
 		res.add = new Function(addFn);
 		child.push(res);
