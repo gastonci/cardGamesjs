b451f4045bff473634bff3b7b698fcbc8607d231
Alejandro Hernández <picanteverde@gmail.com>
2013-02-14 02:05:56 -0300

    scope and identifiers

--- card.js
+++ card.js
@@ -1,25 +1,18 @@
-var Card = function (x, y, z, src) {
+var Card = function (value, suit, css, src) {
     this.size = null;
-    this.value = x;
-    this.suit = y;
+    this.value = value;
+    this.suit = suit;
     this.src = src;
-    this.name = y ? (x + ' of ' + y) : x;
-    this.css = y ? (z + y + x) : z + x;
+    this.name = suit ? (value + ' of ' + suit) : value;
+    this.css = suit ? (css + suit + value) : css + value;
 };
 Card.prototype.setSize = function (a, b, c, d) {
     this.size = [a, b, c, d];
 };
 /*
-when using pseudo constructors for prototypal inheritance, 
-it's best to initialize the object no funcional members 
-in the constructor and define the functional members as 
-members of the prototype of the constructor, this way 
-only one function object is created and shared accross all 
-the instances, instead of creating a copy of the function 
-objects for each functional member for each instance.
-True, avoiding this might have performance boost because 
-the prototype chain is never been searched, all the 
-functions are just right there, but you have to ask 
-your self: what happens more often in my program? object 
-instanciation? or, method calls? and then optimize for that.
+be generous with the identifiers, and remember you don't 
+have implicit scope (unless you use with to extend the 
+scope chain) so if you want to access the "value" member 
+of the "this" object you have to explicitly describe it 
+by "this.value"
 */
